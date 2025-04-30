3376209a61fe4ab433512c169337903d6b62ee77
atecm25 <atecm@miumg.edu.gt>
2025-04-29 20:59:08 -0600

    SEGUNDA PARTE CODIGO FASE 2 AM

--- Traductor.cpp
+++ Traductor.cpp
@@ -45,5 +45,42 @@ int main() {
 
     string linea;
     vector<string> codigoOriginal;
+    while (getline(cin, linea) && !linea.empty()) {
+        codigoOriginal.push_back(linea);
+    }
+
+    stack<string> estructuras;
+    cout << "___ CODIGO TRADUCIDO ____" << endl << endl;
+    string ultimaEstructura = "";
+
+   for (size_t i = 0; i < codigoOriginal.size(); ++i) {
+    string linea = codigoOriginal[i];
+    bool esFunc = esFuncion(linea);
+    bool llaveAbierta = false;
+
+    stringstream nuevaLinea;
+    string palabraActual;
+
+    for (size_t j = 0; j < linea.length(); ++j) {
+        char c = linea[j];
+
+        if (isspace(c) || ispunct(c)) {
+            if (!palabraActual.empty()) {
+                string limpio = palabraActual;
+                limpio.erase(remove_if(limpio.begin(), limpio.end(), ::ispunct), limpio.end());
+
+                if (diccionario.count(limpio)) {
+                    nuevaLinea << diccionario[limpio];
+                    if (limpio == "if" || limpio == "else" || limpio == "while" || limpio == "for" || limpio == "switch") {
+                        ultimaEstructura = diccionario[limpio];
+                        estructuras.push(ultimaEstructura);
+
+                    }
+                } else {
+                    nuevaLinea << palabraActual;
+                }
+
+                palabraActual.clear();
+            }
 
 
